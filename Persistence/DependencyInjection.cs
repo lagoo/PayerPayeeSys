7c0dc45bd3b3850a6be856780522d13867a486ed
Iago Gibin Stanchese <iagogs@gmail.com>
2021-08-03 15:25:57 -0300

    Melhora na implementação do contexto. Possibilidade de usar banco em memoria para teste.

--- Persistence/DependencyInjection.cs
+++ Persistence/DependencyInjection.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Persistence.Extensions;
 
 namespace Persistence
 {
@@ -9,11 +11,20 @@ namespace Persistence
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<ApplicationContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("ApplicationDatabase")));
+            services.Configure<DbConnectionConfig>(options => configuration.GetSection("DbConnection").Bind(options));
+            
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationContext>(options =>
+                    options.UseInMemoryDatabase("PayerPayeeSys"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationContext>((provider, options) => { 
+                    options.SetDbContextOptions(provider.GetRequiredService<IOptions<DbConnectionConfig>>().Value); });                
+            }
 
-            services.AddScoped<IApplicationContext>(provider => provider.GetService<ApplicationContext>());
-            services.AddDbContext<ApplicationContext>();
+            services.AddScoped<IApplicationContext>(provider => provider.GetService<ApplicationContext>());            
 
             return services;
         }
---
+++ Persistence/Extensions/DbContextOptionsBuilderExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistence.Extensions
+{
+    public static class DbContextOptionsBuilderExtension
+    {
+        public static DbContextOptionsBuilder SetDbContextOptions(this DbContextOptionsBuilder options, DbConnectionConfig config)
+        {
+            string dbConnection = config.GetConnectionString();
+
+            return config.Type.ToLower() switch
+            {
+                "sqlserver" => options.UseSqlServer(dbConnection),
+                _ => throw new ArgumentException("Banco não suportado pela aplicação.")
+            };
+        }
+    }
+}
